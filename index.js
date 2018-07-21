f00a570a69ba025b8e5fadbcdf892bc815e082be
Sean Christopher Febo <scf@techie.com>
2018-07-21 02:18:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector); 
+}
+
+function nestedTarget() { 
+  return document.getElementById(`nested`).querySelector(`.target`);
+}
+
+function increaseRankBy(n) { 
+  const listItems = document.getElementById(`app`).querySelectorAll(`.ranked-list li`);
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].innerHTML = parseInt(listItems[i].innerHTML) + n; 
+  }
+}
+  
+function deepestChild() { 
+  return document.querySelector(`#grand-node div div div div`);
+}
