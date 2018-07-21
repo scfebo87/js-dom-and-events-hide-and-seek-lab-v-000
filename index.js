fc495f9b2c14c7d499a2947cb29fbab78a5bbe88
Sean Christopher Febo <scf@techie.com>
2018-07-21 01:49:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector); 
+}
+
+function nestedTarget() { 
+  return document.getElementById(`nested`).querySelector(`.target`);
+}
+
+function deepestchild
